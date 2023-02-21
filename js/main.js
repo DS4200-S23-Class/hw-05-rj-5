663dae44c82358573cd5e036f0554af38acbf7a9
Ruhan Xia <xia.ruh@northeastern.edu>
2023-02-21 00:20:15 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-console.log('linked')
-
 // Declare constants
 const FRAME_HEIGHT = 500;
 const FRAME_WIDTH = 500;
@@ -57,7 +55,7 @@ function build_plot(){
 		}
 	
 		function handleMouseleave() {
-			d3.select(this).style('fill', 'green');
+			d3.select(this).style('fill', 'lightseagreen');
 		}
 
 		function handleMouseclick(d) {
@@ -97,7 +95,86 @@ function build_plot(){
 	})
 }
 
-
-
 build_plot();
+
+const FRAME2 = d3.select('#vis2')
+		          		.append('svg')
+		            		.attr('height', FRAME_HEIGHT)
+		            		.attr('width', FRAME_WIDTH)
+		            		.attr('class', 'bar-chart');
+function bar_plot() {
+
+	d3.csv('data/bar-data.csv').then((data) => {
+
+		const MAX_Y2 = d3.max(data, (d) => {
+		    				return parseInt(d.amount)
+		    				});
+
+		const scaleX = d3.scaleBand()
+							.domain(data.map((d) => {return d.category}))
+		  					.range([0, VIS_WIDTH])
+		  					.padding(0.2);
+
+		const scaleY = d3.scaleLinear()
+		            		.domain([0, MAX_Y2])
+		            		.range([VIS_HEIGHT, 0]);
+
+		FRAME2.selectAll('bars')
+		      	.data(data)
+			    .enter()
+			    .append('rect')
+			        .attr('x', (d) => {
+			          return (scaleX(d.category) + MARGINS.top)})
+			        .attr('y', (d) => {
+			          return (scaleY(d.amount) + MARGINS.left)})
+			        .attr('width', scaleX.bandwidth())
+			        .attr('height', (d) => {
+			          return (VIS_HEIGHT - scaleY(d.amount))})
+			        .attr('class', 'bar');
+
+		FRAME2.append("g")
+				.attr('transform', 'translate(' + MARGINS.left + "," + 
+						(VIS_HEIGHT + MARGINS.top) + ')')
+				.call(d3.axisBottom(scaleX).ticks(10))
+						.attr('font-size', "20px");
+
+		FRAME2.append("g")
+				.attr('transform', 'translate(' + MARGINS.top + "," + 
+						MARGINS.left + ')')
+				.call(d3.axisLeft(scaleY).ticks(6))
+						.attr('font-size', "20px");
+
+		const TOOLTIP = d3.select('#vis2')
+						.append('div')
+							.attr('class', 'tooltip')
+							.style('opacity', 0);
+
+		function handleMouseover() {
+			d3.select(this).style("fill", 'lightsalmon');
+			TOOLTIP.style('opacity', 1);
+
+		}
+	
+		function handleMouseleave() {
+			d3.select(this).style('fill', 'lightseagreen');
+			TOOLTIP.style('opacity', 0);
+
+		}
+
+		function handleMousemove(event, d) {
+			TOOLTIP.html('Category: ' + d.category + '<br>Value: ' + d.amount)
+					.style('left', (event.pageX + 10) + 'px')
+					.style('top', (event.pageY - 50) + 'px');
+		}
+
+		// add event listeners to the points
+		FRAME2.selectAll('.bar')
+				.on('mouseover', handleMouseover)
+				.on('mouseleave', handleMouseleave)
+				.on('mousemove', handleMousemove)
+	})
+}
+
+bar_plot();
+
 
